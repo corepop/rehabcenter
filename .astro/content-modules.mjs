31c31353c393f8fff81569a9bc55c16f03408718
corepop <sebastian.ahman@protonmail.com>
2025-10-01 05:25:31 +0200

    update gitignore

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/data/post/ryggskott.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fpost%2Fryggskott.mdx&astroContentModuleFlag=true")]]);
+		
