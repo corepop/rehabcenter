14e98d70a4b751b75cd277ac0a887c4346a747d6
corepop <sebastian.ahman@protonmail.com>
2025-10-04 05:49:43 +0200

    add node.js for server rendering

--- astro.config.ts
+++ astro.config.ts
@@ -15,6 +15,8 @@ import astrowind from './vendor/integration';
 
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
 
+import node from '@astrojs/node';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const hasExternalScripts = false;
@@ -96,4 +98,8 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
